60e30d4809b5ba96d5fc1107fd9867f77f77908e
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-10-22 23:32:18 +0530

    Server working fine

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -10,8 +10,10 @@ const generateJwt = (input) =>{
 //middleware to verify routes using token
 const authenticateJwt = (req,res,next) =>{
   console.log("entered in jwtauthentication");
-  const authHeader = req.headers.authorization;
-  console.log("authHeader: " + authHeader);
+   const authHeader = req.headers.authorization;
+
+  console.log("authHeader: " , authHeader);
+ 
     if (authHeader) {
       const token = authHeader.split(' ')[1]; // split authHeader into starting from space in it & take 1st //string  after space
       console.log("Token from jwtAuthentication",token);
